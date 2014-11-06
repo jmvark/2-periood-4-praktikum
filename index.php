355c59474e7d68168e266ad29ab2ad31a7d79ccb
JasonMax_2 <jason-max.vark@khk.ee>
2014-11-06 10:50:20 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<DOCTYPE! HTML>
+<html>
+  <head>
+  	<title>Praktikum4</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type"text/css" href="style.css">
+  </head>
+  <body>
+  	dfghj
+  </body>
+</html>
